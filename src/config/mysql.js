e4f0f1384a8ae6ae44a97a87bd22834380120696
lingongbing <lingongbing@enmonster.com>
2022-10-18 15:26:11 +0800

    * add

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -3,14 +3,24 @@ import env from './env'
 
 // 下面的特定环境可以深度合并到上面的默认环境
 // 线上环境是上面的默认环境，不要乱改哦
+// 测试环境：
+// rm-uf6aw5vkvp7zi049y6o.mysql.rds.aliyuncs.com （外网地址） 3306
+// 库名：yoga
+// yoga_test|gaY0fff@1017
+
+
+// 正式环境：
+// quj1htc8qo65kpp3lh3y-rw4rm.rwlb.rds.aliyuncs.com （内外代理） 3306
+// 库名：yoga
+// yoga|gaY01IL@1017
 
 // 开发环境配置
 const development = {
     /* nomal */
-    host: '127.0.0.1',
+    host: 'rm-uf6aw5vkvp7zi049y6o.mysql.rds.aliyuncs.com',
     port: '3306',
-    user: 'root',
-    password: 'root',
+    user: 'yoga_test',
+    password: 'gaY0fff@1017',
     database: 'yoga'
 }
 // 测试环境配置
